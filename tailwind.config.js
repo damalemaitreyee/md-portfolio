640de6e4f0aabeebb05e6c39d1453fe43ba02d02
Maitreyee <maitreyee8818@gmail.com>
2024-02-12 19:00:20 -0600

    es6 syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  variants: {
-    width: ["responsive", "hover", "focus"],
-  },
-  theme: {
-    extend: {
-      backgroundImage: {
-        hero: "url('./images/particle.gif')",
-        img: "linear-gradient(60deg, #29323c 0%, #485563 100%),url('./images/img-13.gif')",
-      },
+export const content = ["./src/**/*.{html,js}"];
+export const variants = {
+  width: ["responsive", "hover", "focus"],
+};
+export const theme = {
+  extend: {
+    backgroundImage: {
+      hero: "url('./images/particle.gif')",
+      img: "linear-gradient(60deg, #29323c 0%, #485563 100%),url('./images/img-13.gif')",
     },
   },
-  plugins: [],
 };
+export const plugins = [];
