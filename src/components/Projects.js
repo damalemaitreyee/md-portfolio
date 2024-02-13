8538f85eea96cb17852e73d62383274d01c9630e
Maitreyee <maitreyee8818@gmail.com>
2024-02-12 23:23:17 -0600

    images change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,26 +12,26 @@ const Projects = () => {
         </h1>
       </div>
       <div className="flex flex-wrap items-center justify-center mt-10">
-        <div className=" bg-img bg-cover rounded-xl flex flex-wrap items-center justify-center flex-row h-96">
+        <div className="bg-slate-500 bg-cover rounded-xl flex flex-wrap items-center justify-center flex-row h-96">
           <div className="flex flex-wrap items-center justify-center basis-1/2 h-full">
             <h1 className="text-white text-3xl">Youtube Clone</h1>
             <br />
-            <p className="text-white text-lg font-light p-2">
+            <p className="text-white text-xl font-light p-2 text-center">
               {" "}
-              Developed a robust food delivery application using MERN stack,
-              Redux, and Git for collaboration; integrated frontend (React.js,
-              Material UI) with backend (Node.js, Express.js, MongoDB) for
-              authentication, orders, and restaurants, enriched with Google APIs
-              for location-based search and a distinctive multi-restaurant
-              purchase feature.
+              Developed a Youtube Clone application using React, Redux, and Git
+              for collaboration; integrated frontend (React.js, TailwindCSS).
+              Implemented Search Suggestion using Youtube Search API.
               <br />
               <b>Tools used:</b> ReactJS, Redux, TailwindCSS
             </p>
-            <a href="https://github.com/hardiknahata/CS5610-DoorDelights-Frontend">
+            <a
+              href="https://github.com/hardiknahata/CS5610-DoorDelights-Frontend"
+              className="text-white "
+            >
               See Project
             </a>
           </div>
-          <div className="bg-img bg-cover rounded-xl basis-1/2 scale-100 h-full hover:scale-110">
+          <div className="bg-slate-700 bg-cover rounded-xl basis-1/2 scale-100 h-full hover:scale-110">
             <img
               className="h-full w-full rounded-lg"
               src={youtube}
@@ -41,48 +41,55 @@ const Projects = () => {
         </div>
         <div className="bg-img bg-cover rounded-xl flex flex-wrap items-center justify-center flex-row h-96">
           <div className="bg-img bg-cover rounded-xl basis-1/2 scale-100 h-full hover:scale-110">
-            <img className="h-full w-full rounded-lg" src={foodorder} />
+            <img
+              className="h-full w-full rounded-lg"
+              src={foodorder}
+              alt="Food Ordering"
+            />
           </div>
           <div className="flex flex-wrap items-center justify-center basis-1/2  h-full">
             <h1 className="text-white text-3xl">Food Ordering App</h1>
             <br />
-            <p className="text-white text-lg font-light p-2">
+            <p className="text-white text-xl font-light p-2 text-center">
               {" "}
-              Developed a robust food delivery application using MERN stack,
-              Redux, and Git for collaboration; integrated frontend (React.js,
-              Material UI) with backend (Node.js, Express.js, MongoDB) for
-              authentication, orders, and restaurants, enriched with Google APIs
-              for location-based search and a distinctive multi-restaurant
-              purchase feature.
+              Developed a robust food delivery application using React, Redux,
+              and Git for collaboration; integrated frontend (React.js, Tailwind
+              CSS).
               <br />
               <b>Tools used:</b> ReactJS, Redux, TailwindCSS
             </p>
-            <a href="https://github.com/hardiknahata/CS5610-DoorDelights-Frontend">
+            <a
+              href="https://github.com/hardiknahata/CS5610-DoorDelights-Frontend"
+              className="text-white "
+            >
               See Project
             </a>
           </div>
         </div>
-        <div className="bg-img bg-cover rounded-xl flex flex-wrap items-center justify-center flex-row h-96">
+        <div className="bg-slate-500 bg-cover rounded-xl flex flex-wrap items-center justify-center flex-row h-96">
           <div className="flex flex-wrap items-center justify-center basis-1/2 h-full">
             <h1 className="text-white text-3xl">Netflix Clone</h1>
             <br />
-            <p className="text-white text-lg font-light p-2">
+            <p className="text-white text-xl font-light p-2 text-center">
               {" "}
-              Developed a robust food delivery application using MERN stack,
-              Redux, and Git for collaboration; integrated frontend (React.js,
-              Material UI) with backend (Node.js, Express.js, MongoDB) for
-              authentication, orders, and restaurants, enriched with Google APIs
-              for location-based search and a distinctive multi-restaurant
-              purchase feature.
+              Developed a Netflix Clone application using React JS, Redux, and
+              Git for collaboration; integrated frontend (React.js, CSS).
               <br />
-              <b>Tools used:</b> ReactJS, Redux, TailwindCSS
+              <b>Tools used:</b> ReactJS, Redux, CSS
             </p>
-            <a href="https://github.com/hardiknahata/CS5610-DoorDelights-Frontend">
+            <a
+              href="https://damalemaitreyee.github.io/netflix-clone/"
+              className="text-white "
+            >
               See Project
             </a>
           </div>
           <div className="bg-img bg-cover rounded-xl basis-1/2 scale-100 h-full hover:scale-110">
-            <img className="h-full w-full rounded-lg" src={netflix} />
+            <img
+              className="h-full w-full rounded-lg"
+              src={netflix}
+              alt="netflix"
+            />
           </div>
         </div>
       </div>
